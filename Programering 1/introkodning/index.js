e286e42e534fe7557f24a922fbd8b4160fb529d0
Max.Magnusson2IT19A <max.magnusson2@elev.ga.ntig.se>
2020-09-01 09:41:31 +0200

    ja

--- Programering 1/introkodning/index.js
+++ Programering 1/introkodning/index.js
@@ -2,4 +2,15 @@
 
 let name = "Max"
 //bröh monento
-console.log(`Hej ${name}`)
+let age = 16
+
+console.log(`Hej ${name} du är ${age} år gammal`)
+
+if (age >=18){
+    conslose.log("du är vuxen")
+} else {
+     }     console.log("haha du är 16")
+
+
+let text= document. querySelector("#text")
+text.innerText = `${name} är ${age} år gammal`
