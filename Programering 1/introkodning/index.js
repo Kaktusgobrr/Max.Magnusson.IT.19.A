c1e1abcba484c21b7a7723d57b2355341d1d04c7
Max.Magnusson2IT19A <max.magnusson2@elev.ga.ntig.se>
2020-09-01 14:10:54 +0200

    hej

--- Programering 1/introkodning/index.js
+++ Programering 1/introkodning/index.js
@@ -11,6 +11,8 @@ if (age >=18){
 } else {
      }     console.log("haha du är 16")
 
-
+if (age >=18){
+} else {
+} console.log("Linus är jätte fin")
 let text= document. querySelector("#text")
 text.innerText = `${name} är ${age} år gammal`
