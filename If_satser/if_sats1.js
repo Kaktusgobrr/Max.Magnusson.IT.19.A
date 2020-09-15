d4e625f86b87e72a3b591730557000fd65618f38
Max.Magnusson2IT19A <max.magnusson2@elev.ga.ntig.se>
2020-09-15 14:59:27 +0200

    hej

---
+++ If_satser/if_sats1.js
@@ -0,0 +1,22 @@
+//modulusoperator %=modulus
+//console.log(`${25%7}`)
+
+let tal = prompt("ange ett tal")
+//let tal = 25
+
+console.log(`${tal}`)
+
+
+if (tal%2 === 0){
+    console.log("jämnt")
+} else{
+    console.log("udda")
+}
+
+if (tal > 0){
+    console.log("positivt")
+} else if (tal == 0){
+    console.log("talet är 0")
+} else {
+    console.log("negativt")
+}
