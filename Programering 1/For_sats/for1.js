ba62d39eca874ff6cacf9ab052594ffb729d9fcf
Max.Magnusson2IT19A <max.magnusson2@elev.ga.ntig.se>
2020-10-01 09:53:24 +0200

    hej

---
+++ Programering 1/For_sats/for1.js
@@ -0,0 +1,38 @@
+let p_talserie1 = document.querySelector("#talserie1");
+let p_talserie2 = document.querySelector("#talserie2");
+let p_summa = document.querySelector("#summa");
+let p_lektioner = document.querySelector("#lektioner");
+let summa = 0;
+
+for (let i = 1; i <= 10; i++) {
+  p_talserie1.innerHTML += `${i} `;
+}
+
+for (let J = 10; J > 0; J--) {
+  p_talserie2.innerHTML += `${J} `;
+}
+
+for (let i = 0; i <= 10; i++) {
+  summa += i;
+}
+p_summa.innerHTML += `1 + 2 + 3 +...+ 9 + 10`;
+console.log(summa);
+
+for (let i = 0; i < 100; i++) {
+  console.log(`${i} i will write all over the wall`);
+}
+
+var lektioner = [
+  "nätverksteknik",
+  "datorteknik",
+  "tillämpladprogramering",
+  "engelska",
+  "svenska",
+  "programering",
+];
+var text = "hej";
+var i;
+for (i = 0; i < lektioner.length; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("lektioner").innerHTML = text;
